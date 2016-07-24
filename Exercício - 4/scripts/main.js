0023fc2f6099dfc1b654abbd9703e2262879c761
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-07-24 15:10:44 -0300

    first commit

---
+++ Exercício - 4/scripts/main.js
@@ -0,0 +1,10 @@
+/*
+ * Este é o JavaScript Scratchpad.
+ *
+ * Escreva JavaScript, e depois clique com o botão direito ou selecione no menu Execução:
+ * 1. Executar: para avaliar o texto selecionado (Ctrl+R),
+ * 2. Inspecionar: para abrir o resultado em uma janela de inspeção (Ctrl+I)
+ * 3. Visualizar: para inserir o resultado em um comentário depois da seleção. (Ctrl+L)
+ */
+
+alert("");
