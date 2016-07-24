e7de20093f4bb07388c54e5c80f841538cfdd55e
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-07-24 20:31:08 -0300

    adicionando exercícios sobre console em JavaScript

---
+++ Exercício - 5/scripts/main.js
@@ -0,0 +1,16 @@
+/*
+ * Este é o JavaScript Scratchpad.
+ *
+ * Escreva JavaScript, e depois clique com o botão direito ou selecione no menu Execução:
+ * 1. Executar: para avaliar o texto selecionado (Ctrl+R),
+ * 2. Inspecionar: para abrir o resultado em uma janela de inspeção (Ctrl+I)
+ * 3. Visualizar: para inserir o resultado em um comentário depois da seleção. (Ctrl+L)
+ */
+
+2+2;
+/*
+4
+*/
+console.log("Olá Glaucia Lemos!!!");
+console.warn("Alerta!!!!");
+console.error("Error!");
