f73035479e1ea53475fc0b2d19924c4817bf1e76
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-07-24 22:56:23 -0300

    O desafio do FizzBuzz mais detalhado em Js

---
+++ Exercício - 11/scripts/main.js
@@ -0,0 +1,25 @@
+/*
+ * Este é o JavaScript Scratchpad.
+ *
+ * Escreva JavaScript, e depois clique com o botão direito ou selecione no menu Execução:
+ * 1. Executar: para avaliar o texto selecionado (Ctrl+R),
+ * 2. Inspecionar: para abrir o resultado em uma janela de inspeção (Ctrl+I)
+ * 3. Visualizar: para inserir o resultado em um comentário depois da seleção. (Ctrl+L)
+ */
+
+var randomNumber = 15;
+
+if(randomNumber % 3 === 0 && randomNumber != 0) {
+  if(randomNumber % 5 === 0) {
+    alert("fizzbuzz");
+  }
+  else {
+    alert("fizz");
+  }
+}
+else if(randomNumber % 5 === 0 && randomNumber != 0) {
+  alert("buzz");
+}
+else {
+  console.log(randomNumber);
+}
