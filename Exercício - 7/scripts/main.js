fd9d1a25ba4535834568821fd9d6e1ec29f95c33
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-07-24 21:42:41 -0300

    exercício sobre declaração de variáveis em Js.

---
+++ Exercício - 7/scripts/main.js
@@ -0,0 +1,15 @@
+/*
+ * Este é o JavaScript Scratchpad.
+ *
+ * Escreva JavaScript, e depois clique com o botão direito ou selecione no menu Execução:
+ * 1. Executar: para avaliar o texto selecionado (Ctrl+R),
+ * 2. Inspecionar: para abrir o resultado em uma janela de inspeção (Ctrl+I)
+ * 3. Visualizar: para inserir o resultado em um comentário depois da seleção. (Ctrl+L)
+ */
+
+var a = 2;
+var b = 4;
+
+var myName = "Glaucia Lemos";
+alert(myName);
+
