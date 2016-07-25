6ec45df755b79b34c4e02d388454f787a1369637
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-07-24 21:12:27 -0300

    exercícios em Javascript sobre tipos de dados primitivos

---
+++ Exercício - 6/scripts/game.js
@@ -0,0 +1,57 @@
+/*
+ * Este é o JavaScript Scratchpad.
+ *
+ * Escreva JavaScript, e depois clique com o botão direito ou selecione no menu Execução:
+ * 1. Executar: para avaliar o texto selecionado (Ctrl+R),
+ * 2. Inspecionar: para abrir o resultado em uma janela de inspeção (Ctrl+I)
+ * 3. Visualizar: para inserir o resultado em um comentário depois da seleção. (Ctrl+L)
+ */
+
+300/2;
+/*
+150
+*/
+typeof(300/2);
+/*
+number
+*/
+
+"Olá" + " Glaucia";
+/*
+Olá Glaucia
+*/
+typeof("Olá" + " Glaucia");
+/*
+string
+*/
+
+4 < 2;
+/*
+false
+*/
+typeof(4 < 2);
+/*
+boolean
+*/
+
+a + b;
+/*
+Exception: ReferenceError: a is not defined
+@Scratchpad/4:37:1
+*/
+typeof(a + b);
+/*
+Exception: ReferenceError: a is not defined
+@Scratchpad/4:37:1
+*/
+
+3 + "6";
+/*
+Exception: ReferenceError: a is not defined
+@Scratchpad/4:37:1
+*/
+typeof(3 + "6");
+/*
+Exception: ReferenceError: a is not defined
+@Scratchpad/4:37:1
+*/
---
+++ Exercício - 6/scripts/main.js
@@ -0,0 +1,39 @@
+/*
+ * Este é o JavaScript Scratchpad.
+ *
+ * Escreva JavaScript, e depois clique com o botão direito ou selecione no menu Execução:
+ * 1. Executar: para avaliar o texto selecionado (Ctrl+R),
+ * 2. Inspecionar: para abrir o resultado em uma janela de inspeção (Ctrl+I)
+ * 3. Visualizar: para inserir o resultado em um comentário depois da seleção. (Ctrl+L)
+ */
+
+2+2
+/*
+4 --> tipo: number
+*/
+
+2 + "2"
+/*
+22 --> tipo: string
+*/
+
+"Oi Glaucia" + " Lemos";
+/*
+Oi Glaucia Lemos --> string concatenada
+*/
+
+10 > 2;
+/*
+true --> tipo boolean
+*/
+
+10 < -2;
+/*
+false --> tipo boolean
+*/
+
+typeof(10 < 2);
+/*
+boolean --> 'typeof' serve para saber qual o tipo de tipo estamos codificando
+*/
+
